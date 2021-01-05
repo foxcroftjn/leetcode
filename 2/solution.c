89327d7f8e26d3ec5f074888ee8e8a1cd1873ee3
Jeremy Foxcroft <foxcroftjn@gmail.com>
2021-01-04 17:08:05 -0700

    Refactor Add Two Numbers solution

--- 2/solution.c
+++ 2/solution.c
@@ -5,27 +5,22 @@ struct ListNode{
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
     struct ListNode *result = malloc(sizeof(struct ListNode));
-    struct ListNode *pointer = result;
+    struct ListNode *pointer = NULL;
     int carry = 0;
-    while(1){
-        int sum = l1 ? l2 ? l1->val + l2->val : l1->val : l2->val;
-        sum += carry;
-        carry = sum / 10;
-        sum %= 10;
-        pointer->val = sum;
-        if((l1 && l1->next) || (l2 && l2->next)){
-            if(l1) l1 = l1->next;
-            if(l2) l2 = l2->next;
+    do{
+        if(pointer){
             pointer->next = malloc(sizeof(struct ListNode));
             pointer = pointer->next;
         }else{
-            if(carry){
-                pointer->next = malloc(sizeof(struct ListNode));
-                pointer = pointer->next;
-                pointer->val = carry;
-            }
-            pointer->next = NULL;
-            return result;
+            pointer = result;
         }
-    }
+        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+        carry = sum / 10;
+        sum %= 10;
+        pointer->val = sum;
+        if(l1) l1 = l1->next;
+        if(l2) l2 = l2->next;
+    }while(l1 || l2 || carry);
+    pointer->next = NULL;
+    return result;
 }
